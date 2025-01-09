84f7139b5c1e21613ae825bb0904aebe36d1a47f
SangamithraMB <159931656+SangamithraMB@users.noreply.github.com>
2025-01-09 13:33:34 +0100

    UserList

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,27 @@ import SportList from './components/SportList';
 
 function App() {
   return (
-    <div className="bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBqG5u3QHbkq7W2_cZygxKL7vMHhsMP4NUKw&s')] bg-cover bg-center h-64 w-full">
-      <h1 className=" flex center text-3xl font-bold underline">Sport Buddy App</h1>
-      <UserList />
-      <SportList />
+    <div className='w-full h-screen bg-gray-100 relative'>
+      
+        <div 
+          className="absolute top-0 right-0 m-4"> 
+          <img src="src/assets/basketball_ring.png"
+          alt='Basketball ring'
+          className='w-32 h-32 object-contain'
+          />  
+        </div>
+        <div 
+          className="absolute bottom-0 left-0 m-4"> 
+          <img src="src/assets/basketball.png"
+          alt='Basketball'
+          className='w-74 h-364 object-contain'
+          />  
+        </div>
+      <div className="text-center">
+      <h1 className="text-3xl font-bold text-blue p-4">Sport Buddy App</h1>
+        <UserList />
+        <SportList />
+      </div>
     </div>
   );
 }
