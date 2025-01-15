751701d88e20c78da9f55c9f065c9e54021b56f9
SangamithraMB <159931656+SangamithraMB@users.noreply.github.com>
2025-01-15 17:43:43 +0100

    chhanges in Navbar included sports

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ function Navbar() {
       <ul className="flex justify-around">
         <li><Link to="/" className="text-white">Home</Link></li>
         <li><Link to="/users" className="text-white">Users</Link></li>
+        <li><Link to="/sports" className="text-white">Sports</Link></li>
         <li><Link to="/playdates" className="text-white">Playdates</Link></li>
         <li><Link to="/add-user" className="text-white">Add User</Link></li>
       </ul>
