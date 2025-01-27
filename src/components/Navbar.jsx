d6db2ee65cddac30a22bfa5edb8adad4f7d47d9f
SangamithraMB <159931656+SangamithraMB@users.noreply.github.com>
2025-01-27 10:01:46 +0100

    changes in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,19 @@
-// components/Navbar.jsx
 import { Link } from 'react-router-dom';
+import { useAuth } from './AuthContext';
 
 function Navbar() {
+  const {logout} = useAuth();
   return (
     <nav className="bg-blue-500 p-4">
-      <ul className="flex justify-around">
-        <li><Link to="/" className="text-white">Home</Link></li>
-        <li><Link to="/users" className="text-white">Users</Link></li>
-        <li><Link to="/sports" className="text-white">Sports</Link></li>
-        <li><Link to="/playdates" className="text-white">Playdates</Link></li>
-        <li><Link to="/add-user" className="text-white">Add User</Link></li>
+      <ul className="flex justify-around space-x-4">
+        <li><Link to="/" className="text-white hover:text-gray-300">Home</Link></li>
+        <li><Link to="/login" className="text-white hover:text-gray-300">Login</Link></li>
+        <li><Link to="/users" className="text-white hover:text-gray-300">Users</Link></li>
+        <li><Link to="/sports" className="text-white hover:text-gray-300">Sports</Link></li>
+        <li><Link to="/open-playdates" className="text-white hover:text-gray-300">Open Playdates</Link></li>
+        <li><Link to="/playdates" className="text-white hover:text-gray-300">Create Playdate</Link></li>
+        <li><Link to="/add-user" className="text-white hover:text-gray-300">Sign-Up</Link></li>
+        <li><button onClick={() => logout()} className="text-white hover:text-gray-300">Logout</button></li>
       </ul>
     </nav>
   );
