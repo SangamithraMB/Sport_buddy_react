d424566f988d8ee28bd25dae79c16e898d034ef9
SangamithraMB <159931656+SangamithraMB@users.noreply.github.com>
2025-01-28 12:38:28 +0100

    tailwind update for user registration

--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -10,6 +10,8 @@ function CreateUser() {
   const [password, setPassword] = useState("");
   const [userCreated, setUserCreated] = useState(null);
   const [message, setMessage] = useState("");
+  const [formVisible, setFormVisible] = useState(true); 
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -25,7 +27,8 @@ function CreateUser() {
     try {
       const result = await createUser(userData);
       setUserCreated(result);
-      setMessage("User created successfully!");
+      setMessage(""); 
+      setFormVisible(false); 
 
       setFirstName("");
       setLastName("");
@@ -33,98 +36,102 @@ function CreateUser() {
       setEmail("");
       setPassword("");
     } catch (err) {
-      setMessage("Failed to create user!", err);
+      console.error(err);
+      setMessage("Failed to create user!");
     }
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-6 bg-white shadow-lg rounded-lg">
-        <h2 className="text-2xl font-bold mb-6 text-center text-blue-600">
-          Create User
-        </h2>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <div className="space-y-1">
-            <label className="block text-gray-700">First Name:</label>
-            <input
-              type="text"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-              placeholder="Enter First Name"
-              className="w-full p-2 border border-gray-300 rounded-md"
-              required
-            />
+    <div className="flex justify-center items-center min-h-screen bg-red-100 bg-[url(/assets/racetrack.avif)] bg-blend-overlay bg-cover bg-no-repeat bg-center">
+      <div className="w-full max-w-md p-6 bg-purple bg-opacity-25 rounded-lg shadow-lg transition-all duration-500">
+        {formVisible && (
+          <div>
+            <h2 className="text-2xl font-bold mb-6 text-center text-blue-600">
+              User Registration
+            </h2>
+            <form onSubmit={handleSubmit} className="space-y-4">
+              <div className="space-y-1">
+                <label className="block text-gray-700">First Name:</label>
+                <input
+                  type="text"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  placeholder="Enter First Name"
+                  className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  required
+                />
+              </div>
+              <div className="space-y-1">
+                <label className="block text-gray-700">Last Name:</label>
+                <input
+                  type="text"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  placeholder="Enter Last Name"
+                  className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  required
+                />
+              </div>
+              <div className="space-y-1">
+                <label className="block text-gray-700">User Name:</label>
+                <input
+                  type="text"
+                  value={userName}
+                  onChange={(e) => setUserName(e.target.value)}
+                  placeholder="Enter User Name"
+                  className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  required
+                />
+              </div>
+              <div className="space-y-1">
+                <label className="block text-gray-700">Email:</label>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Enter Email"
+                  className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  required
+                />
+              </div>
+              <div className="space-y-1">
+                <label className="block text-gray-700">Password:</label>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Enter Password"
+                  className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  required
+                />
+              </div>
+              <div className="mt-4">
+                <button
+                  type="submit"
+                  className="w-full py-2 px-4 bg-gradient-to-r from-green-400 to-blue-500 text-white rounded-md shadow-lg hover:from-blue-500 hover:to-green-400 transform transition duration-300 ease-in-out hover:scale-105"
+                >
+                  Register
+                </button>
+              </div>
+              {message && (
+                <p className="mt-4 text-center text-red-600">{message}</p>
+              )}
+            </form>
           </div>
-          <div className="space-y-1">
-            <label className="block text-gray-700">Last Name:</label>
-            <input
-              type="text"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-              placeholder="Enter Last Name"
-              className="w-full p-2 border border-gray-300 rounded-md"
-              required
-            />
-          </div>
-          <div className="space-y-1">
-            <label className="block text-gray-700">User Name:</label>
-            <input
-              type="text"
-              value={userName}
-              onChange={(e) => setUserName(e.target.value)}
-              placeholder="Enter User Name"
-              className="w-full p-2 border border-gray-300 rounded-md"
-              required
-            />
-          </div>
-          <div className="space-y-1">
-            <label className="block text-gray-700">Email:</label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Enter Email"
-              className="w-full p-2 border border-gray-300 rounded-md"
-              required
-            />
-          </div>
-          <div className="space-y-1">
-            <label className="block text-gray-700">Password:</label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Enter Password"
-              className="w-full p-2 border border-gray-300 rounded-md"
-              required
-            />
-          </div>
-          <div className="mt-4">
-            <button
-              type="submit"
-              className="w-full py-2 px-4 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition duration-200"
-            >
-              Register
-            </button>
-          </div>
-          {message && (
-            <p className="mt-4 text-center text-red-600">{message}</p>
-          )}
-        </form>
-        {userCreated ? (
-          <div className="mt-4 text-center">
-            <h3 className="font-bold text-green-600">
-              User created successfully:
+        )}
+
+        {userCreated && !formVisible && (
+          <div className="fade-in text-center mt-6">
+            <h3 className="font-bold text-green-600 mb-4">
+              User created successfully!
             </h3>
-            <p>
-              {userCreated.first_name} {userCreated.last_name} (
-              {userCreated.username})
-            </p>
+            <Link to="/">
+              <button className="mt-4 px-6 py-2 bg-gradient-to-r from-blue-600 to-blue-800 text-white rounded-md shadow-lg hover:from-blue-800 hover:to-blue-600 transform transition duration-300 ease-in-out hover:scale-105">
+                Go to Home
+              </button>
+            </Link>
           </div>
-        ) : null}
-        <Link to="/">
-        <button className="mt-4 px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition duration-200">Go to Home </button>
-      </Link>
+        )}
       </div>
     </div>
   );
