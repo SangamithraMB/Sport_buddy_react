fed0938c3c4e7b9d991c76f83e098e40a6191a41
SangamithraMB <159931656+SangamithraMB@users.noreply.github.com>
2025-02-12 12:19:28 +0100

    updating route

--- src/Services/apiService.js
+++ src/Services/apiService.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// const API_BASE_URL = 'https://sport-buddy-test.onrender.com'; 
-const API_BASE_URL = 'http://127.0.0.1:5000';
+const API_BASE_URL = 'https://sport-buddy-test.onrender.com'; 
 
 const api = axios.create({
   baseURL: API_BASE_URL,
