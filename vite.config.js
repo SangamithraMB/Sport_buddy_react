1321915ab0bab26cd627efb3b95d836896ecec14
SangamithraMB <159931656+SangamithraMB@users.noreply.github.com>
2025-03-21 09:29:12 +0100

    MODIFIED README.md

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Allow all hosts
+    allowedHosts: ['sport-buddy-app-render.onrender.com'], // Add Render domain to allowed hosts
+  },
 });
